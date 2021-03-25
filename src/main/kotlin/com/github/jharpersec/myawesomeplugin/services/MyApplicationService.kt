d688661bc190ea65df3bf13168e1aad5a94cbf76
GitHub Action <action@github.com>
2021-03-25 12:50:33 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/jharpersec/myawesomeplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.jharpersec.myawesomeplugin.services
+
+import com.github.jharpersec.myawesomeplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
